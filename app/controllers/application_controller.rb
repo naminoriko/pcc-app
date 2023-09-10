840871c01511b6d23eb80b24b7a39c01c6b95cd7
naminoriko <dankan1003@gmail.com>
2023-09-10 21:42:00 +0900

    basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ptcttc' && password == '5476'
+    end
+  end
+
+ 
+
+
+
+
+
 end
