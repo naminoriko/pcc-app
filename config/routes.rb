cb5e6ab68c8a9fea83c7e8486c7fc1428f5246da
naminoriko <dankan1003@gmail.com>
2023-09-13 10:19:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
  # get 'schedules/index'
   root to: "schedules#index"  
-  resources :users, only: [:show, :edit, :update]
+  resources :users,     only: [:show, :edit, :update]
+  resources :schedules, only: [:new, :create] #do
+    #resources :applicants, only: [:index?, :create?] 
+  #end
 
 end
