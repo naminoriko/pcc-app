58dad0807b593bc4c8b24dd9a80bd00338722ce1
naminoriko <dankan1003@gmail.com>
2023-09-14 17:24:04 +0900

    applicantモデル作成、マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_13_141233) do
+ActiveRecord::Schema.define(version: 2023_09_14_072522) do
+
+  create_table "applicants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "schedule_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["schedule_id"], name: "index_applicants_on_schedule_id"
+    t.index ["user_id"], name: "index_applicants_on_user_id"
+  end
 
   create_table "disciplines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -49,5 +59,7 @@ ActiveRecord::Schema.define(version: 2023_09_13_141233) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applicants", "schedules"
+  add_foreign_key "applicants", "users"
   add_foreign_key "schedules", "users"
 end
